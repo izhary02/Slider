b880d02a0ab9997831586727c8a13b94405b2a5d
izhar yefet <izhar.yefet@gmail.com>
2023-12-26 03:18:28 +0200

    Slider

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Carousel from './Carousel'
+import SlickCarousel from './SlickCarousel'
+
+const App = () => {
+  return (
+    <main>
+      {/* Build alone the Carousel */}
+      {/* <Carousel /> */}
+
+      {/* Build the Carousel using external folder "React Slick"*/}
+      <SlickCarousel />
+    </main>
+  )
+}
+export default App
